1f16a72308a18b28da5a450b14d1433a0cdcd8f4
jh.kim <88549117+kjeongh@users.noreply.github.com>
2023-04-02 12:55:34 -0600

    [level 1] Title: [1차] 비밀지도, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -3,18 +3,10 @@ def solution(n, arr1, arr2):
     
     # 각 배열의 원소를 하나씩 꺼내어 or연산
     or_arr = [bin(arr1[i] | arr2[i])[2:].zfill(n) for i in range(n)]
+    print(or_arr)
     
     # 1인 부분은 #, 0인 부분은 공백으로 변환하기
-    for i in range(n):
-        map = ""
-        for j in range(n):
-            c = or_arr[i][j]
-            print(c)
-            if c == '1':
-                map = map + '#'
-            else:
-                map = map + ' '
-        answer.append(map)
-    
+    for i in or_arr:
+        answer.append(i.replace('1','#').replace('0',' '))
 
     return answer
